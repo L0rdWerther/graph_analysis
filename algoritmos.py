87a8c739a448b324cbe70c35ef14afedd68ce5e5
LucasTNM <148718831+LucasTNM@users.noreply.github.com>
2025-11-08 10:48:01 -0300

    Gitignore and default config

--- algoritmos.py
+++ algoritmos.py
@@ -1,1 +1,20 @@
+from collections import deque
 
+def bfs(grafo, inicio):
+    visitado = [False]*grafo.n
+    nivel = [-1]*grafo.n
+    pai = [-1]*grafo.n
+
+    fila = deque([inicio])
+    visitado[inicio] = True
+    nivel[inicio] = 0
+
+    while fila:
+        u = fila.popleft()
+        for v in grafo.lista_adj[u]:
+            if not visitado[v-1]:
+                visitado[v-1] = True
+                pai[v-1] = u
+                nivel[v-1] = nivel[u-1] + 1
+                fila.append(v)
+    return pai, nivel
--- grafo.py
+++ grafo.py
@@ -1,1 +1,14 @@
+class Grafo:
+    def __init__(self, n):
+        self.n = n
+        self.lista_adj = [[] for _ in range(n)]
+        self.matriz_adj = [[0]*n for _ in range(n)]
 
+    def adicionar_aresta(self, u, v):
+        self.lista_adj[u-1].append(v)
+        self.lista_adj[v-1].append(u)
+        self.matriz_adj[u-1][v-1] = 1
+        self.matriz_adj[v-1][u-1] = 1
+
+    def grau(self, v):
+        return len(self.lista_adj[v-1])
